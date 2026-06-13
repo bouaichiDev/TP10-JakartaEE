af0d537cd9dfd95d625ac4d4ea9c6223e6ca78d1
Badr <bouaichibadr@gmail.com>
2026-06-13 11:51:12 +0100

    first commit

---
+++ src/main/java/ma/fstg/security/spring_security_demo/web/AuthController.java
@@ -0,0 +1,30 @@
+package ma.fstg.security.spring_security_demo.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard() {
+        return "admin-dashboard";
+    }
+
+    @GetMapping("/user/dashboard")
+    public String userDashboard() {
+        return "user-dashboard";
+    }
+
+}
